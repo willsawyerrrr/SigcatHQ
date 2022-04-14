2953d809e30205e10ee82275603d988f4b399a6f
William Sawyer <w.sawyer@uqconnect.edu.au>
2022-04-14 16:39:47 +0000

    implement input/output loop
    git-svn-id: https://source.eait.uq.edu.au/svn/csse2310-sem1-s4696360/trunk/a3@82 a1968c47-03bd-fd43-9cb2-c192510db353

---
+++ sigcat.c
@@ -0,0 +1,16 @@
+#include <csse2310a3.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char* input;
+    FILE* outputStream = stdout;
+
+    do {
+        input = read_line(stdin);
+        fprintf(outputStream, input);
+        free(input);
+    } while (!feof(stdin));
+
+    return 0;
+}
