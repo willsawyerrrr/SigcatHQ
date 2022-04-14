f3fe859fbdd701346fb7dc360d7a2c03d54f5faa
William Sawyer <w.sawyer@uqconnect.edu.au>
2022-04-14 18:18:37 +0000

    create sigactions for SIGUSR signals to change output stream
    git-svn-id: https://source.eait.uq.edu.au/svn/csse2310-sem1-s4696360/trunk/a3@87 a1968c47-03bd-fd43-9cb2-c192510db353

--- sigcat.c
+++ sigcat.c
@@ -1,6 +1,7 @@
 #include "sigcat.h"
 
 #include <csse2310a3.h>
+#include <signal.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -10,6 +11,18 @@ FILE* outputStream;
 int main() {
     char* input;
     outputStream = stdout;
+
+    struct sigaction toStdout;
+    memset(&toStdout, 0, sizeof(toStdout));
+    toStdout.sa_handler = setStreamStdout;
+    toStdout.sa_flags = SA_RESTART;
+    sigaction(SIGUSR1, &toStdout, 0);
+
+    struct sigaction toStderr;
+    memset(&toStderr, 0, sizeof(toStderr));
+    toStderr.sa_handler = setStreamStderr;
+    toStderr.sa_flags = SA_RESTART;
+    sigaction(SIGUSR2, &toStderr, 0);
 
     do {
         input = read_line(stdin);
