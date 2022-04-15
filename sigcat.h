cde64b3f2caa3d028289a81da58b65a528801fb6
William Sawyer <w.sawyer@uqconnect.edu.au>
2022-04-15 06:03:20 +0000

    add documentation to method signatures
    git-svn-id: https://source.eait.uq.edu.au/svn/csse2310-sem1-s4696360/trunk/a3@93 a1968c47-03bd-fd43-9cb2-c192510db353

--- sigcat.h
+++ sigcat.h
@@ -1,4 +1,13 @@
+/**
+ * Sets the handler for all signals (1 to 31) to the below handler method.
+ */
 void setHandlers();
 
+/**
+ * Print the following text:
+ *     sigcat received <signal name>
+ * to the current output stream, where <signal name> is replaced by
+ * the signal name reported by strsignal().
+ */
 void handler(int signum);
 
