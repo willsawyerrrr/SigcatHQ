69311e3d6cb2e482de01a8bd09cc44667c8fe6a4
William Sawyer <w.sawyer@uqconnect.edu.au>
2022-05-02 19:17:01 +0000

    separated child structures and child-related functions
    git-svn-id: https://source.eait.uq.edu.au/svn/csse2310-sem1-s4696360/trunk/a3@150 a1968c47-03bd-fd43-9cb2-c192510db353

---
+++ child.c
@@ -0,0 +1,103 @@
+#include "child.h"
+
+#include <csse2310a3.h>
+#include <ctype.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+// longest possible status is null-terminated "signalled(XX)" (14 characters)
+#define MAX_STATUS_BUFFER_SIZE 14
+
+/* Stores information about child processes created by the spawn command. */
+extern ChildList* childList;
+
+Child* get_child_by_jobid(int jobId) {
+    Child** children = childList->children;
+    for (int i = 0; children[i]; i++) {
+        if (children[i]->jobId == jobId) {
+            return children[i];
+        }
+    }
+
+    return NULL;
+}
+
+Child* get_child_by_pid(int processId) {
+    Child** children = childList->children;
+    for (int i = 0; children[i]; i++) {
+        if (children[i]->processId == processId) {
+            return children[i];
+        }
+    }
+
+    return NULL;
+}
+
+ChildList* init_child_list() {
+    ChildList* childList = malloc(sizeof(ChildList));
+    childList->numChildren = 0;
+    Child** children = malloc(sizeof(Child));
+    childList->children = children;
+    return childList;
+}
+
+Child* init_child(pid_t processId, char* programName, int pToC, int cToP) {
+    // create child
+    Child* child = malloc(sizeof(Child));
+    child->processId = processId;
+    child->jobId = childList->numChildren;
+    child->programName = malloc(strlen(programName) + 1);
+    strcpy(child->programName, programName);
+    child->status = malloc(MAX_STATUS_BUFFER_SIZE);
+    strcpy(child->status, "running");
+    child->pToC = pToC;
+    child->cToP = cToP;
+        
+    // add the child to the list
+    childList->children = realloc(childList->children,
+            sizeof(Child) * (++(childList->numChildren)));
+    childList->children[childList->numChildren - 1] = child;
+
+    return child;
+}
+
+void wait_on_child(Child* child) {
+    int statusCode;
+    char* status = child->status;
+    
+    // only write wait on child and write to its status string if the last
+    // status was running
+    if (!strcmp(status, "running")
+            && waitpid(child->processId, &statusCode, WNOHANG)) {
+        if (WIFEXITED(statusCode)) { // statusCode => exited
+            sprintf(status, "exited(%d)", WEXITSTATUS(statusCode));
+        } else if (WIFSIGNALED(statusCode)) { // statusCode => signalled
+            sprintf(status, "signalled(%d)", WTERMSIG(statusCode));
+        } else {
+            strcpy(status, "running");
+        }
+    }
+}
+
+void reap_child(int signum, siginfo_t* info, void* ucontext) {
+    Child* child = get_child_by_pid(info->si_pid);
+    wait_on_child(child);
+}
+
+void free_child_list() {
+    Child** children = childList->children;
+    for (int i = 0; children[i]; i++) {
+        Child* child = children[i];
+        free(child->programName);
+        free(child->status);
+        free(child);
+    }
+    free(children);
+    free(childList);
+}
+
--- hq.h
+++ hq.h
@@ -1,39 +1,10 @@
 #ifndef HQ_H
 #define HQ_H
 
+#include "child.h"
+
 #include <signal.h>
 #include <sys/types.h>
-
-/*
- * Structure type to hold a child process - created within spawn.
- */
-typedef struct {
-    /* Process ID of the newly created child, relative to the kernel. */
-    pid_t processId;
-    /* Job ID of the newly created child, relative to hq. */
-    pid_t jobId;
-    /* Name of the program running within the child process. */
-    char* programName;
-    /* Status of the child process, as used within the report command.  */
-    char* status;
-    /* File descriptor for the pipe used to write from the parent (this hq
-     * process) to this child process. */
-    int pToC;
-    /* File descriptor for the pipe used to read from this child within its
-     * parent (this hq process).
-     */
-    int cToP;
-} Child;
-
-/*
- * Structure type to holf all children processes created by spawn.
- */
-typedef struct {
-    /* The number of children processes this process has spawned. */
-    int numChildren;
-    /* Array of pointers to the children processes this process has spawned. */
-    Child** children;
-} ChildList;
 
 /*
  * Sets handlers to ignore the interrupt and broken pipe signals and reap
@@ -258,59 +229,6 @@ int validate_jobid(char* jobId);
  * Returns 1 (true) if and only if arg is a valid signal number; 0 otherwise.
  */
 int validate_signum(char* signum);
-
-/*
- * Returns the child process specified by the given job ID.
- *
- * If no child has the given job ID, NULL is returned.
- */
-Child* get_child_by_jobid(int jobId);
-
-/*
- * Returns the child process specified by the given process ID.
- *
- * If no child has the process job ID, NULL is returned.
- */
-Child* get_child_by_pid(int processId);
-
-/*
- * Returns a malloc()'d pointer to an empty child list with count
- * numChildren = 0.
- *
- * The caller of this function is responsible for freeing the memory allocated
- * for the child list and all its children.
- */
-ChildList* init_child_list();
-
-/*
- * Creates and returns a new Child struct with the given process ID, job ID,
- * program name and pipe file descriptors and adds it to the given ChildList.
- *
- * The caller of this function is responsible for freeing the memory allocated
- * for the child.
- */
-Child* init_child(pid_t processId, char* programName, int pToC, int cToP);
-
-/*
- * Waits on the given child, reaping it and urassigning the appropriate string
- * to the child's status member for use in report().
- *
- * If the status string has been changed from the initial "running", the child
- * must have been waited on after being either exited or signalled and hence is
- * not waited on again.
- */
-void wait_on_child(Child* child);
-
-/*
- * Reaps the child which initiated the SIGCHLD signal which caused this
- * function call. Handler function only.
- */
-void reap_child(int signum, siginfo_t* info, void* ucontext);
-
-/*
- * Frees the given ChildList and all of its children.
- */
-void free_child_list();
 
 #endif
 
